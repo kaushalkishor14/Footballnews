cf68f88a74de4697b1cff2528c8f495ea618912b
Kaushal <kaushalkishorroy21@gmail.com>
2022-09-21 23:05:17 +0530

    day-2

--- newsapp/src/Components/NavBar.js
+++ newsapp/src/Components/NavBar.js
@@ -10,7 +10,7 @@ export class NavBar extends Component {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
-              Classic-Cars
+              Footballnews Goo
             </a>
             <button
               className="navbar-toggler"
